693da3427944bf60d6e1a4e7ed076b62a3def3d4
Noor696 <eng.nooralhateeb@gmail.com>
2023-02-11 13:16:38 +0000

    ChooseYourOwnAdventure

---
+++ ChooseYourOwnAdventure.py
@@ -0,0 +1,45 @@
+name = input("\nType your name: ")
+print("Welcome", name , "to this adventure")
+
+answer = input("\nYou are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? (left/right)").lower()
+
+if answer == "left":
+    answer = input("You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: (swim/walk)").lower()
+
+    if answer == "swim":
+        print ('You swam acrross and were eaten by an alligator.')
+
+
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water and you lost the game.")
+
+    else : 
+        print ('Not a valid option. You lose.')
+    
+
+elif answer == "right":
+    answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ").lower()
+
+    if answer == "back":
+        print("You go back and lose.")
+
+    elif answer == "cross":
+        answer = input("You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ").lower()
+        
+        if answer == "yes":
+             print("You talk to the stanger and they give you gold. You WIN!")
+
+        elif answer == "no":
+             print("You talk to the stanger and they give you gold. You WIN!")
+
+        else :
+            print ('Not a valid option. You lose.')
+
+    else:
+        print ('Not a valid option. You lose.')
+
+
+else: 
+    print('Not a valid option. You lose.')
+
+print("Thank you for trying", name)
